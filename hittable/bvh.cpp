730c3357f9630aa31fea1ad058f51b769274c207
pthav <pta2@rice.edu>
2025-08-09 14:50:43 -0700

    Made BVH construction iterative

--- hittable/bvh.cpp
+++ hittable/bvh.cpp
@@ -1,4 +1,63 @@
 #include "bvh.h"
+#include <algorithm>
+#include <queue>
+
+BVH::BVH(HittableList list)
+    : m_objects{list.getObjects()},
+      m_aabb{list.boundingBox()}
+{
+    size_t bitLength{0};
+    size_t x{m_objects.size()};
+    for (; x != 0; x >>= 1)
+    {
+        bitLength++;
+    }
+    m_nodes.resize(1 << (bitLength + 1));
+
+    std::queue<work> queue{};
+    queue.push(work{1, 0, m_objects.size()});
+
+    while (!queue.empty())
+    {
+        work w{queue.front()};
+        queue.pop();
+
+        auto aabb{AABB::empty};
+        for (size_t i = w.m_start; i < w.m_end; i++)
+        {
+            aabb = AABB(aabb, m_objects[i]->boundingBox());
+        }
+        m_nodes[w.m_node].m_aabb = aabb;
+
+        int axis = aabb.longestAxis();
+
+        auto comparator = (axis == 0)
+                              ? boxXCompare
+                              : (axis == 1)
+                                    ? boxYCompare
+                                    : boxZCompare;
+
+        size_t span{w.m_end - w.m_start};
+        size_t left = {w.m_node * 2};
+        size_t right{w.m_node * 2 + 1};
+        if (span == 1)
+        {
+            m_nodes[left] = m_nodes[right] = {m_objects[w.m_start]->boundingBox(), true, w.m_start};
+        } else if (span == 2)
+        {
+            m_nodes[left] = {m_objects[w.m_start]->boundingBox(), true, w.m_start};
+            m_nodes[right] = {m_objects[w.m_start + 1]->boundingBox(), true, w.m_start + 1};
+        } else
+        {
+            std::sort(std::begin(m_objects) + w.m_start, std::begin(m_objects) + w.m_end,
+                      comparator);
+
+            auto mid = w.m_start + span / 2;
+            queue.push(work{w.m_node * 2, w.m_start, mid});
+            queue.push(work{w.m_node * 2 + 1, mid, w.m_end});
+        }
+    }
+}
 
 bool BVH::hit(const ray &r, Interval rayT, hitRecord &rec) const
 {
@@ -7,8 +66,62 @@ bool BVH::hit(const ray &r, Interval rayT, hitRecord &rec) const
         return false;
     }
 
-    bool hitLeft = m_left->hit(r, rayT, rec);
-    bool hitRight = m_right->hit(r, Interval(rayT.m_min, hitLeft ? rec.m_t : rayT.m_max), rec);
+    bool hitSomething{false};
+    double closest {rayT.m_max};
+    std::queue<size_t> queue{};
+    queue.push(2);
+    queue.push(3);
 
-    return hitLeft || hitRight;
+    while (!queue.empty())
+    {
+        auto idx {queue.front()};
+        queue.pop();
+
+        auto node {m_nodes[idx]};
+        if (!node.m_leaf)
+        {
+            if (node.m_aabb.hit(r, rayT))
+            {
+                queue.emplace(idx * 2);
+                queue.emplace(idx * 2 + 1);
+            }
+        } else
+        {
+            if (m_objects[node.m_object]->hit(r, Interval(rayT.m_min, closest), rec))
+            {
+                hitSomething = true;
+                closest = rec.m_t;
+            }
+        }
+    }
+    return hitSomething;
+}
+
+[[nodiscard]] AABB BVH::boundingBox() const
+{
+    return m_aabb;
+}
+
+bool BVH::boxCompare(
+    const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b, int axis_index
+)
+{
+    auto a_axis_interval = a->boundingBox().axisInterval(axis_index);
+    auto b_axis_interval = b->boundingBox().axisInterval(axis_index);
+    return a_axis_interval.m_min < b_axis_interval.m_min;
+}
+
+bool BVH::boxXCompare(const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b)
+{
+    return boxCompare(a, b, 0);
+}
+
+bool BVH::boxYCompare(const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b)
+{
+    return boxCompare(a, b, 1);
+}
+
+bool BVH::boxZCompare(const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b)
+{
+    return boxCompare(a, b, 2);
 }
--- hittable/bvh.h
+++ hittable/bvh.h
@@ -5,83 +5,43 @@
 #include "hittable.h"
 #include "hittable_list.h"
 
-#include <algorithm>
+struct node
+{
+    AABB m_aabb{AABB::empty};
+    bool m_leaf{false};
+    size_t m_object{};
+};
 
+struct work
+{
+    size_t m_node{};
+    size_t m_start{};
+    size_t m_end{};
+};
 
 class BVH : public Hittable
 {
 public:
-    explicit BVH(HittableList list) : BVH(list.getObjects(), 0, list.getObjects().size())
-    {
-    }
-
-    BVH(std::vector<std::shared_ptr<Hittable> > &objects, size_t start, size_t end)
-    {
-        // Build the bounding box of the span of source objects.
-        m_aabb = AABB::empty;
-        for (size_t object_index = start; object_index < end; object_index++)
-            m_aabb = AABB(m_aabb, objects[object_index]->boundingBox());
-
-        int axis = m_aabb.longestAxis();
-
-        auto comparator = (axis == 0)
-                              ? boxXCompare
-                              : (axis == 1)
-                                    ? boxYCompare
-                                    : boxZCompare;
-
-        size_t object_span = end - start;
-
-        if (object_span == 1)
-        {
-            m_left = m_right = objects[start];
-        } else if (object_span == 2)
-        {
-            m_left = objects[start];
-            m_right = objects[start + 1];
-        } else
-        {
-            std::sort(std::begin(objects) + static_cast<int>(start), std::begin(objects) + static_cast<int>(end),
-                      comparator);
-
-            auto mid = start + object_span / 2;
-            m_left = make_shared<BVH>(objects, start, mid);
-            m_right = make_shared<BVH>(objects, mid, end);
-        }
-    }
+    explicit BVH(HittableList list);
 
     bool hit(const ray &r, Interval rayT, hitRecord &rec) const override;
 
-    [[nodiscard]] AABB boundingBox() const override { return m_aabb; }
+    [[nodiscard]] AABB boundingBox() const override;
 
 private:
-    std::shared_ptr<Hittable> m_left;
-    std::shared_ptr<Hittable> m_right;
-    AABB m_aabb;
+    std::vector<std::shared_ptr<Hittable>> m_objects;
+    std::vector<node> m_nodes{};
+    AABB m_aabb{};
 
     static bool boxCompare(
         const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b, int axis_index
-    )
-    {
-        auto a_axis_interval = a->boundingBox().axisInterval(axis_index);
-        auto b_axis_interval = b->boundingBox().axisInterval(axis_index);
-        return a_axis_interval.m_min < b_axis_interval.m_min;
-    }
+    );
 
-    static bool boxXCompare(const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b)
-    {
-        return boxCompare(a, b, 0);
-    }
+    static bool boxXCompare(const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b);
 
-    static bool boxYCompare(const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b)
-    {
-        return boxCompare(a, b, 1);
-    }
+    static bool boxYCompare(const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b);
 
-    static bool boxZCompare(const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b)
-    {
-        return boxCompare(a, b, 2);
-    }
+    static bool boxZCompare(const std::shared_ptr<Hittable> &a, const std::shared_ptr<Hittable> &b);
 };
 
 
