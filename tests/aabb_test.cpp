85f6f71284457a549453ce3fe3f03ffbb2f9abb9
pthav <pta2@rice.edu>
2025-08-10 16:47:15 -0700

    Added some tests to interval_test.cpp and aabb_test.cpp

--- tests/aabb_test.cpp
+++ tests/aabb_test.cpp
@@ -6,10 +6,19 @@
 class AABBTest : public testing::Test
 {
 protected:
+    static double error;
+
     static void CheckEqualsInterval(Interval interval1, Interval interval2, std::string_view message)
     {
-        EXPECT_DOUBLE_EQ(interval1.m_start, interval2.m_start) << message << "failed for the start";
-        EXPECT_DOUBLE_EQ(interval1.m_end, interval2.m_end) << message << "failed for the end";
+        EXPECT_NEAR(interval1.m_min, interval2.m_min,error) << message << "failed for the start";
+        EXPECT_NEAR(interval1.m_max, interval2.m_max,error) << message << "failed for the end";
+    }
+
+    static void CheckEqualsAABB(const AABB &aabb1, const AABB &aabb2, std::string_view message)
+    {
+        CheckEqualsInterval(aabb1.axisInterval(0), aabb2.axisInterval(0), message);
+        CheckEqualsInterval(aabb1.axisInterval(1), aabb2.axisInterval(1), message);
+        CheckEqualsInterval(aabb1.axisInterval(2), aabb2.axisInterval(2), message);
     }
 
     static void CheckEqualsVector(const Vec3 &vec1, const Vec3 &vec2, std::string_view message)
@@ -20,59 +29,169 @@ protected:
     }
 };
 
-TEST_F(AABBTest, TestBracketOperator)
+inline double AABBTest::error{0.0001};
+
+TEST_F(AABBTest, TestAABBFromIntervals)
 {
-    Interval x{-5, 2.4};
-    Interval y{4, 5};
-    Interval z{2, 7.5};
-    AABB aabb{x, y, z};
-    CheckEqualsInterval(x, aabb[0], "Bracket operator");
-    CheckEqualsInterval(y, aabb[1], "Bracket operator");
-    CheckEqualsInterval(z, aabb[2], "Bracket operator");
+    struct Test
+    {
+        Interval x;
+        Interval y;
+        Interval z;
+    };
+
+    auto tests{
+        std::vector<Test>{
+            {{0, 2}, {5, 10}, {3, 11}},
+            {{-105.21, -10.56}, {100, 152.2}, {-5, -2}}
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        AABB result{test.x, test.y, test.z};
+        CheckEqualsInterval(test.x, result.axisInterval(0), "AABB From Intervals");
+        CheckEqualsInterval(test.y, result.axisInterval(1), "AABB From Intervals");
+        CheckEqualsInterval(test.z, result.axisInterval(2), "AABB From Intervals");
+    }
 }
 
-TEST_F(AABBTest, TestGetCentroid)
+TEST_F(AABBTest, TestAABBFromPoints)
 {
-    point3 point1{-5, 4, 2};
-    point3 point2{2.4, 5, 7.5};
-    AABB aabb{point1, point2};
-    CheckEqualsVector(aabb.centroid(), Vec3{-1.3, 4.5, 4.75}, "Get Centroid");
+    struct Test
+    {
+        point3 point1;
+        point3 point2;
+        AABB expected;
+    };
+
+    auto tests{
+        std::vector<Test>{
+            {point3{0, 2, 1}, point3{-1, 5, 13}, AABB{Interval{-1, 0}, Interval{2, 5}, Interval{1, 13}}},
+            {point3{-5, -2, 11}, point3{-3, 5, 12}, AABB{Interval{-5, -3}, Interval{-2, 5}, Interval{11, 12}}},
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        CheckEqualsAABB(test.expected, AABB{test.point1, test.point2}, "AABB From Points");
+        CheckEqualsAABB(test.expected, AABB{test.point2, test.point1}, "AABB From Points");
+    }
 }
 
-TEST_F(AABBTest, TestSurfaceArea)
+TEST_F(AABBTest, TestMergeAABBWithPoint)
 {
-    Interval x{-5, 2.4};
-    Interval y{4, 5};
-    Interval z{2, 7.5};
-    AABB aabb{x, y, z};
-    EXPECT_DOUBLE_EQ(aabb.surfaceArea(),
-                     x.size() * y.size() + y.size() * z.size() + z.size() * x.size()) << "Surface area incorrect";
-}
+    struct Test
+    {
+        AABB aabb;
+        point3 point;
+        AABB expected;
+    };
 
-TEST_F(AABBTest, TestExpand)
-{
-    double delta{0.0001};
-    Interval x{0, 0};
-    Interval y{1, 1};
-    Interval z{2, 2};
-    AABB aabb{x, y, z};
+    auto tests{
+        std::vector<Test>{
+            {
+                AABB{Interval{-5, 5}, Interval{3, 12.1}, Interval{-101.2, 12.5}}, point3{-6, -4, -103},
+                AABB{Interval{-6, 5}, Interval{-4, 12.1}, Interval{-103, 12.5}}
+            },
+            {
+                AABB{Interval{-5, 5}, Interval{3, 12.1}, Interval{-101.2, 12.5}}, point3{6, 13, 13},
+                AABB{Interval{-5, 6}, Interval{3, 13}, Interval{-101.2, 13}}
+            },
+        }
+    };
 
-    CheckEqualsInterval(aabb[0], Interval{x.m_start - delta / 2.0, x.m_end + delta / 2.0}, "Expand interval");
-    CheckEqualsInterval(aabb[1], Interval{y.m_start - delta / 2.0, y.m_end + delta / 2.0}, "Expand interval");
-    CheckEqualsInterval(aabb[2], Interval{z.m_start - delta / 2.0, z.m_end + delta / 2.0}, "Expand interval");
+    for (const auto &test: tests)
+    {
+        CheckEqualsAABB(test.expected, AABB{test.aabb, test.point}, "Merge AABB with Point");
+        CheckEqualsAABB(test.expected, AABB{test.point, test.aabb}, "Merge AABB with Point");
+    }
 }
 
 TEST_F(AABBTest, TestMergeAABB)
 {
-    point3 p1{-5, 4, 2};
-    point3 p2{2.4, 5, 7.5};
-    AABB aabb1{p1, p2};
-    point3 p3{-3, -4, 1};
-    point3 p4{17.4, -13, 1.5};
-    AABB aabb2{p3, p4};
+    struct Test
+    {
+        AABB aabb1;
+        AABB aabb2;
+        AABB expected;
+    };
 
-    AABB merged{aabb1, aabb2};
-    CheckEqualsInterval(merged[0],Interval{-5,17.4},"Merge AABB x");
-    CheckEqualsInterval(merged[1],Interval{-13,5},"Merge AABB y");
-    CheckEqualsInterval(merged[2],Interval{1,7.5},"Merge AABB z");
+    auto tests{
+        std::vector<Test>{
+            {
+                AABB{point3{-5, 4, 2}, point3{2.4, 5, 7.5}}, AABB{point3{-3, -4, 1}, point3{17.4, -13, 1.5}},
+                AABB{Interval{-5, 17.4}, Interval{-13, 5}, Interval{1, 7.5}},
+            }
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        CheckEqualsAABB(test.expected, AABB{test.aabb1, test.aabb2}, "Merging AABBs");
+        CheckEqualsAABB(test.expected, AABB{test.aabb2, test.aabb1}, "Merging AABBs");
+    }
+}
+
+TEST_F(AABBTest, TestAxisInterval)
+{
+    Interval x{-5, 2.4};
+    Interval y{4, 5};
+    Interval z{2, 7.5};
+    AABB aabb{x, y, z};
+    CheckEqualsInterval(x, aabb.axisInterval(0), "Bracket operator");
+    CheckEqualsInterval(y, aabb.axisInterval(1), "Bracket operator");
+    CheckEqualsInterval(z, aabb.axisInterval(2), "Bracket operator");
+}
+
+TEST_F(AABBTest, TestSurfaceArea)
+{
+    struct Test
+    {
+        Interval x;
+        Interval y;
+        Interval z;
+    };
+
+    auto tests{
+        std::vector<Test>{
+            {{-5, 2.4}, {4, 5}, {2, 7.5}},
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        AABB aabb{test.x, test.y, test.z};
+        EXPECT_DOUBLE_EQ(aabb.surfaceArea(),
+                         test.x.size() * test.y.size() + test.y.size() * test.z.size() + test.z.size() * test.x.size(
+                         )) << "Surface area incorrect";
+    }
+}
+
+TEST_F(AABBTest, TestPad)
+{
+    double delta{0.0001};
+    struct Test
+    {
+        Interval x;
+        Interval y;
+        Interval z;
+    };
+
+    auto tests{
+        std::vector<Test>{
+            {{0, 0}, {1, 1}, {2, 2}},
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        AABB aabb{test.x, test.y, test.z};
+
+        CheckEqualsAABB(aabb, AABB{
+                            Interval{test.x.m_min - delta / 2.0, test.x.m_max + delta / 2.0},
+                            Interval{test.y.m_min - delta / 2.0, test.y.m_max + delta / 2.0},
+                            Interval{test.z.m_min - delta / 2.0, test.z.m_max + delta / 2.0},
+                        }, "Pad AABB");
+    }
 }
--- tests/interval_test.cpp
+++ tests/interval_test.cpp
@@ -6,23 +6,23 @@ class IntervalTest : public testing::Test
 protected:
     static void CheckEquals(Interval interval1, Interval interval2, std::string_view message)
     {
-        EXPECT_DOUBLE_EQ(interval1.m_start, interval2.m_start) << message << " failed for the start";
-        EXPECT_DOUBLE_EQ(interval1.m_end, interval2.m_end) << message << " failed for the end";
+        EXPECT_DOUBLE_EQ(interval1.m_min, interval2.m_min) << message << " failed for the start";
+        EXPECT_DOUBLE_EQ(interval1.m_max, interval2.m_max) << message << " failed for the end";
     }
 };
 
 TEST_F(IntervalTest, TestSize)
 {
-    Interval interval {12.1,-5.6};
+    Interval interval{-5.6, 12.1};
     EXPECT_DOUBLE_EQ(interval.size(), 12.1 - (-5.6)) << "Size failed";
 }
 
 TEST_F(IntervalTest, TestExpand)
 {
-    Interval interval {10,-5};
-    Interval expanded {interval.expand(0.56f)};
-    EXPECT_DOUBLE_EQ(expanded.m_start, -5 - 0.56f/2.0) << "Expanded failed for start";
-    EXPECT_DOUBLE_EQ(expanded.m_end, 10 + 0.56f/2.0) << "Expanded failed for end";
+    Interval interval{-5, 10};
+    Interval expanded{interval.expand(0.56f)};
+    EXPECT_DOUBLE_EQ(expanded.m_min, -5 - 0.56f/2.0) << "Expanded failed for start";
+    EXPECT_DOUBLE_EQ(expanded.m_max, 10 + 0.56f/2.0) << "Expanded failed for end";
 }
 
 TEST_F(IntervalTest, TestMergeIntervals)
@@ -43,10 +43,133 @@ TEST_F(IntervalTest, TestMergeIntervals)
         }
     };
 
-    for (const auto& test: tests)
+    for (const auto &test: tests)
     {
-        Interval result {test.interval1, test.interval2};
+        Interval result{test.interval1, test.interval2};
         CheckEquals(result, test.expected, "Merge Intervals");
     }
+}
 
+TEST_F(IntervalTest, TestMergeIntervalWithScalar)
+{
+    struct Test
+    {
+        Interval interval;
+        double scalar;
+        Interval expected;
+    };
+
+    auto tests{
+        std::vector<Test>{
+            {Interval{0, 10}, 5, {Interval{0, 10}}},
+            {Interval{-5, 12}, -6, {Interval{-6, 12}}},
+            {Interval{3, 6}, 9, {Interval{3, 9}}},
+            {Interval{0, 10}, -3, {Interval{-3, 10}}},
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        Interval result{test.interval, test.scalar};
+        CheckEquals(result, test.expected, "Merge Intervals");
+
+        result = {test.scalar, test.interval};
+        CheckEquals(result, test.expected, "Merge Intervals");
+    }
+}
+
+TEST_F(IntervalTest, TestContains)
+{
+    struct Test
+    {
+        Interval interval;
+        double scalar;
+        bool contains;
+    };
+
+    auto tests{
+        std::vector<Test>{
+            {{0, 10}, 5, true},
+            {{0, 10}, 10, true},
+            {{0, 10}, 0, true},
+            {{0, 10}, -5, false},
+            {{0, 10}, 15, false}
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        EXPECT_EQ(test.interval.contains(test.scalar), test.contains);
+    }
+}
+
+TEST_F(IntervalTest, TestSurrounds)
+{
+    struct Test
+    {
+        Interval interval;
+        double scalar;
+        bool surrounds;
+    };
+
+    auto tests{
+        std::vector<Test>{
+            {{0, 10}, 5, true},
+            {{0, 10}, 10, false},
+            {{0, 10}, 0, false},
+            {{0, 10}, -5, false},
+            {{0, 10}, 15, false}
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        EXPECT_EQ(test.interval.surrounds(test.scalar), test.surrounds);
+    }
+}
+
+TEST_F(IntervalTest, TestClamp)
+{
+    struct Test
+    {
+        Interval interval;
+        double scalar;
+        double expected;
+    };
+
+    auto tests{
+        std::vector<Test>{
+            {{0, 10}, 5, 5},
+            {{0, 10}, -1, 0},
+            {{0, 10}, 11, 10}
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        EXPECT_DOUBLE_EQ(test.expected, test.interval.clamp(test.scalar));
+    }
+}
+
+TEST_F(IntervalTest, TestDisplacement)
+{
+    struct Test
+    {
+        Interval interval;
+        double displacement;
+        Interval expected;
+    };
+
+    auto tests{
+        std::vector<Test>{
+            {{0, 10}, 2.1,{2.1,12.1}},
+            {{0,10},-1.9,{-1.9,8.1}}
+        }
+    };
+
+    for (const auto &test: tests)
+    {
+        CheckEquals(test.expected, test.interval + test.displacement, "Interval displacement");
+        CheckEquals(test.expected, test.displacement + test.interval, "Interval displacement");
+    }
 }
